ee142867cd0de914f4bf33a0e251caeff78911da
3n0ugh <serhatkarabulut@protonmail.com>
2021-12-02 21:56:41 +0300

    :sparkles: set database connection

---
+++ cmd/web/database/connection.go
@@ -0,0 +1,16 @@
+package database
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func DBOpen(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
